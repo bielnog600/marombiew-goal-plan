4037d729a1d2747af27f003c79cc1d4a0e7092ee
bielnog600 <bielnog600@gmail.com>
2026-09-12 00:29:07 +0100

    AI: Intensifica o degradê sobre a parte inferior da imagem do treinador para disfarçar melhor sua transição com o fundo.

--- src/components/landing/HeroTransformation.tsx
+++ src/components/landing/HeroTransformation.tsx
@@ -64,7 +64,7 @@ const HeroTransformation = ({ profileImage, transformations }: HeroTransformatio
             className="absolute bottom-[31%] left-1/2 z-10 h-[52svh] max-h-[560px] w-auto max-w-[70vw] -translate-x-1/2 object-contain object-bottom drop-shadow-[0_20px_28px_rgba(0,0,0,0.7)]"
             fetchPriority="high"
           />
-          <div className="absolute inset-x-0 bottom-0 z-10 h-[48%] bg-gradient-to-b from-transparent via-[#050505]/75 to-[#050505]" />
+          <div className="absolute inset-x-0 bottom-0 z-10 h-[48%] bg-gradient-to-b from-transparent via-[#050505]/90 to-[#050505]" />
         </>
       )}
 
